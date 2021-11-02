0ace620444fa21a05437b946ccf2b58e40fc5828
adebureaux <adebureaux@gmail.com>
2021-11-02 23:54:50 +0100

    finished

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ with open("dico.txt", 'r') as dico:
 		i += 1
 word = word.rstrip()
 
-print(word)
-
 current = ""
 len = len(word)
 i = 0
@@ -26,20 +24,29 @@ while i < len:
 	i += 1
 current = list(current)
 
-last = 10
+last = 20
 while last > 0:
 	print("Il vous reste", last, "coup(s) a jouer")
-	print(current)
+	print(' '.join(current))
 	print("Tapez une lettre")
 	user_input = "/"
 	while user_input and user_input[0].isalpha() == False:
 		user_input = sys.stdin.readline()
-	if user_input:
-		char = user_input[0]
-	else:
-		char = "/"
-	pos = word.find(char)
-	while pos != -1:
-		current[pos] = char
-		pos = word.find(char)
+	if not user_input:
+		user_input = "/"
+	ret = 0
+	pos = 0
+	while ret != -1:
+		ret = word.find(user_input[0].upper(), pos)
+		if ret != -1:
+			current[ret] = user_input[0].upper()
+			pos += ret + 1
+	if ''.join(current) == word:
+		last = -1
+	print()
 	last -= 1
+if last == -2:
+	print(word)
+	print("Bravo vous avez gagné !")
+else:
+	print("Vous avez perdu :(")
